bf60e9a951bd4c2e951afd62d403caa9c8a05a7b
AkASH jain <aj9882854@gmail.com>
2025-06-18 01:19:49 +0530

    gg

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -204,7 +204,7 @@ const ClothingLanding = () => {
       </p>
     </div>
 
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-12">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
       {featuredProducts.map((product, index) => (
         <div
           key={product.id}
